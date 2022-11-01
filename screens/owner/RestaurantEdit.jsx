c240d2d76d0ec0a0fd205c51c05be71737221554
German <germanaltairac@hotmail.com>
2022-11-01 20:32:37 -0300

    Swiper

--- screens/owner/RestaurantEdit.jsx
+++ screens/owner/RestaurantEdit.jsx
@@ -1,8 +1,10 @@
-import {SafeAreaView, StyleSheet, Text, TextInput, View} from "react-native";
-import {useEffect} from "react";
+import {Button, SafeAreaView, StyleSheet, Text, TextInput, View} from "react-native";
+import {useEffect, useRef} from "react";
 import Swiper from 'react-native-swiper'
 
 export const RestaurantEdit = ({navigation, route}) => {
+
+    const swiper = useRef(null)
 
     const isEdit = () => {
       return true
@@ -14,35 +16,45 @@ export const RestaurantEdit = ({navigation, route}) => {
         })
     }, [navigation])
 
+
+
     return (
-        <Swiper>
-            <View>
-                <Text style={styles.text}>Necesitarás completar los siguientes datos</Text>
-                <TextInput></TextInput>
-                <TextInput></TextInput>
-                <View></View>
-                <TextInput></TextInput>
-            </View>
-            <View>
+        <SafeAreaView style={styles.container}>
+            <Swiper loop={false} ref={swiper}>
                 <View>
-
-                </View>
-                <View>
-
-                </View>
-            </View>
-            <View>
-                <View>
+                    <Text style={styles.text}>Necesitarás completar los siguientes datos</Text>
+                    <TextInput></TextInput>
+                    <TextInput></TextInput>
+                    <View>
+                    </View>
                     <TextInput></TextInput>
                 </View>
-                <View></View>
-                <View></View>
-            </View>
-        </Swiper>
+                <View>
+                    <View>
+
+                    </View>
+                    <View>
+
+                    </View>
+                </View>
+                <View>
+                    <View>
+                        <TextInput></TextInput>
+                    </View>
+                    <View></View>
+                    <View></View>
+                </View>
+            </Swiper>
+            <Button title={"s"} onPress={() => swiper.current.scrollBy(1)}></Button>
+            <Button title={"s"} onPress={() => swiper.current.scrollBy(-1)}></Button>
+        </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
+    container:{
+        flex:1
+    },
     text:{
         color:"black"
     }
